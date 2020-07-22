9518312695614c49f4c67dca455c6aa86c52cbde
SiqiWang666 <siqi_astrid@gwu.edu>
2020-07-22 12:08:10 -0400

    Add cookie authentication(#7).

--- MovieStore.MVC/Controllers/AccountController.cs
+++ MovieStore.MVC/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using MovieStore.Core.Models.Request;
 using MovieStore.Core.ServiceInterfaces;
@@ -45,12 +49,46 @@ namespace MovieStore.MVC.Controllers
             if (ModelState.IsValid)
             {
                 var user = await _userService.ValidateUser(loginRequest.Email, loginRequest.Password);
+                
                 if (user == null)
                 {
+                    // Bug don't return error message and continue next code block, raise ArugumentNullException
                     ModelState.AddModelError(string.Empty, "Invalid login");
+                    // temp solution
+                    return View();
                 }
+                
+                // A claim is a statement about an entity
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.GivenName, user.FirstName),
+                    new Claim(ClaimTypes.Surname, user.LastName),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.Email),
+                };
+                
+                // create an Identity object to hold those claims. A ClaimsIdentity --> identifier of a person
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                
+                // create cookie that will be attached to the Http Response
+                // ToDo search HttpContext, holds all the info regarding that Http request/response
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity));
+                // can manually creating cookie, SigInAsync helps to encrypt the info
+                // HttpContext.Response.Cookies.Append("userlanguage", value);
+                
+                // Once ASP.NET Creates Authentication Cookies, it will check for that cookie in the HttpRequest and see if the cookie is not expired
+                // and it will decrypt the information present in the cookie to check whether User is Authenticated and will also get claims from the cookies
+                
+                return LocalRedirect("~/");
             }
             return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return LocalRedirect("~/");
         }
     }
 }
--- MovieStore.MVC/Startup.cs
+++ MovieStore.MVC/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -50,6 +51,14 @@ namespace MovieStore.MVC
             services.AddScoped<IUserService, UserService>();
 
             services.AddScoped<ICryptoService, CryptoService>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.Cookie.Name = "MovieStoreAuthCookie";
+                    options.ExpireTimeSpan = TimeSpan.FromDays(2);
+                    options.LoginPath = "/Account/Login";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +80,7 @@ namespace MovieStore.MVC
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
