8da119bb7167c62007ec325f35753797a7d913c2
SiqiWang666 <siqi_astrid@gwu.edu>
2020-07-23 13:07:02 -0400

    Authorize User Controller(#9).

--- MovieStore.MVC/Controllers/UserController.cs
+++ MovieStore.MVC/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MovieStore.Core.Entities;
@@ -11,6 +12,7 @@ using MovieStore.Core.ServiceInterfaces;
 
 namespace MovieStore.MVC.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         private readonly IUserService _userService;
@@ -19,7 +21,7 @@ namespace MovieStore.MVC.Controllers
         {
             _userService = userService;
         }
-        // GET
+
         public IActionResult Index()
         {
             return View();
@@ -28,30 +30,24 @@ namespace MovieStore.MVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Favorite(UserFavoriteRequestModel requestModel)
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                var userId = Convert.ToInt32(HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
-                await _userService.FavoriteMovie(requestModel.MovieId, userId);
+            var userId = Convert.ToInt32(HttpContext.User.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            await _userService.FavoriteMovie(requestModel.MovieId, userId);
 
-                return Redirect($"~/Movies/Details/{requestModel.MovieId}");
-            }
-            return RedirectToAction("Login", "Account");
+            return Redirect($"~/Movies/Details/{requestModel.MovieId}");
         }
 
         [HttpPost]
         public async Task<IActionResult> Unfavorite(UserFavoriteRequestModel requestModel)
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                var userId = Convert.ToInt32(HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
-                await _userService.UnfavoriteMovie(requestModel.MovieId, userId);
-                
-                return Redirect($"~/Movies/Details/{requestModel.MovieId}");
-            }
-            return RedirectToAction("Login", "Account");
+            var userId = Convert.ToInt32(HttpContext.User.Claims
+                .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            await _userService.UnfavoriteMovie(requestModel.MovieId, userId);
+
+            return Redirect($"~/Movies/Details/{requestModel.MovieId}");
         }
 
-        [HttpGet("/User/{id?}/Movie/{movieId?}/favorite")]
+        [HttpGet("/User/{id}/Movie/{movieId}/favorite")]
         public async Task IsFavorite(int id, int movieId)
         {
             try
@@ -67,5 +63,14 @@ namespace MovieStore.MVC.Controllers
                 throw;
             }
         }
+
+        /*
+         * Filters (attribute) in ASP.NET. Some piece of code that runs either before an controller or action method executes or when some event happens.
+         * 1. authorization
+         * 2. action filter
+         * 3. result filter
+         * 4. exception filter(only catch in the controller), but in real world, we use exception middleware.
+         * 5. resource filter
+         */
     }
 }
