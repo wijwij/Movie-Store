225c3db0536e635ac2cec00922c8f8110da74eab
SiqiWang666 <siqi_astrid@gwu.edu>
2020-08-11 00:47:52 -0400

    Decode the response token(#21).

--- MovieStoreSPA/src/app/core/services/auth.service.ts
+++ MovieStoreSPA/src/app/core/services/auth.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { Login } from 'src/app/shared/models/login';
 import { JwtStorageService } from 'src/app/core/services/jwt-storage.service';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root',
 })
@@ -22,12 +23,21 @@ export class AuthService {
     return this.apiService.create('account/login', model).pipe(
       map((res) => {
         if (res) {
-          // call jwt-storage to store the token in the local storage
+          // call jwt-storage service to store the token (json body inside the response) in the local storage
           this.jwtStorage.saveToken(res.token);
           // decode the token and populate user info
+          this.populateUserInfo();
           return true;
         } else return false;
       })
     );
+  }
+
+  populateUserInfo() {
+    if (this.jwtStorage.getToken()) {
+      const encodedToken = this.jwtStorage.getToken();
+      const helperService = new JwtHelperService();
+      const decodedToken = helperService.decodeToken(encodedToken);
+    }
   }
 }
