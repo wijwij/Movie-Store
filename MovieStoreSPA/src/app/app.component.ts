1cdc64aabc9bc276564079dd08fafa819981c272
SiqiWang666 <siqi_astrid@gwu.edu>
2020-08-31 14:39:36 -0400

    Emit user state infor at root component(#32).

--- MovieStoreSPA/src/app/app.component.ts
+++ MovieStoreSPA/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'MovieStoreSPA';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.publishUserInfo();
+  }
 }
