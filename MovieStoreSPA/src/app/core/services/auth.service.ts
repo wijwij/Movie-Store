abb399b4aa0a0a90af49bc8d3f2650d9c06f3b35
SiqiWang666 <siqi_astrid@gwu.edu>
2020-08-31 14:38:28 -0400

    Use BehaviorSubject to manage user state(#32).

--- MovieStoreSPA/src/app/core/services/auth.service.ts
+++ MovieStoreSPA/src/app/core/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { SignUp } from 'src/app/shared/models/signup';
 import { User } from 'src/app/shared/models/user';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Login } from 'src/app/shared/models/login';
 import { JwtStorageService } from 'src/app/core/services/jwt-storage.service';
 import { map } from 'rxjs/operators';
@@ -12,6 +12,12 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   providedIn: 'root',
 })
 export class AuthService {
+  // A BehaviorSubject that broadcasts the current user
+  public currentUserSubject = new BehaviorSubject<User>({} as User);
+  // A BehaviorSubject that broadcasts if the user is authenticated
+  public isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  // private user: User;
+
   constructor(
     private apiService: ApiService,
     private jwtStorage: JwtStorageService
@@ -26,8 +32,10 @@ export class AuthService {
       map((res) => {
         // call jwt-storage service to store the token (json body inside the response) in the local storage
         this.jwtStorage.saveToken(res.token);
-        // decode the token and populate user info
-        this.populateUserInfo();
+        // decode the token
+        // this.user = this.decodeToken(res.token);
+        // publish user info
+        this.publishUserInfo();
         return true;
       })
       // Migrate catch failed http response error to the api service.
@@ -36,13 +44,35 @@ export class AuthService {
 
   logout() {
     this.jwtStorage.destroyToken();
+    // Publish new current user state
+    this.currentUserSubject.next({} as User);
+    // Publish new auth status to false
+    this.isAuthenticatedSubject.next(false);
   }
 
-  populateUserInfo() {
-    if (this.jwtStorage.getToken()) {
-      const encodedToken = this.jwtStorage.getToken();
-      const helperService = new JwtHelperService();
-      const decodedToken = helperService.decodeToken(encodedToken);
+  decodeToken(encodedToken: string): User {
+    // decode the token
+    const helperService = new JwtHelperService();
+    if (!encodedToken || helperService.isTokenExpired(encodedToken)) {
+      this.logout();
+      return null;
     }
+    const decodedToken = helperService.decodeToken(encodedToken);
+    return decodedToken;
+  }
+
+  publishUserInfo() {
+    // Publish current user's state
+    if (this.jwtStorage.getToken()) {
+      const token = this.jwtStorage.getToken();
+      const decodedToken = this.decodeToken(token);
+
+      this.currentUserSubject.next(decodedToken);
+      this.isAuthenticatedSubject.next(true);
+    }
+  }
+
+  getCurrentUser(): User {
+    return this.currentUserSubject.value;
   }
 }
