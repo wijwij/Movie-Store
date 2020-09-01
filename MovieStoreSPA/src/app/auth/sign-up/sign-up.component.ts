74b0d714d50f3a1a1e7fd6a2e057d4698b5923fa
SiqiWang666 <siqi_astrid@gwu.edu>
2020-08-31 22:28:13 -0400

    Password validation(#21).

--- MovieStoreSPA/src/app/auth/sign-up/sign-up.component.ts
+++ MovieStoreSPA/src/app/auth/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,13 +13,23 @@ export class SignUpComponent implements OnInit {
     email: new FormControl('', [Validators.email, Validators.required]),
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.minLength(8)),
+    password: new FormControl('', [
+      Validators.minLength(8),
+      Validators.pattern(
+        '(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}'
+      ),
+    ]),
   });
-  constructor(private authService: AuthService) {}
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   onSubmit() {
     console.log(this.registerForm.value);
+    this.authService.register(this.registerForm.value).subscribe(() => {
+      console.log('Successfully register...redirect to login');
+      this.router.navigate(['/login']);
+    });
   }
 }
